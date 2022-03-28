c35add0ec63d600fea24194ecfb21d94878ce61d
Anna Shaposhnikova <Shaposhnikova.anna@gmail.com>
2022-03-28 19:56:58 +0300

    recivers page

--- pages/reciver.vue
+++ pages/reciver.vue
@@ -1,10 +1,195 @@
 <template>
-    <div class="container">
-        <h1>Ресiвери</h1>
+    <div class="container-fluid">
+        <article>
+            <div class="row justify-content-center pb-2 m-2">
+                <h1>
+                    Ресивери для стисненого повітря горизонтальне та вертикальне
+                    виконання
+                </h1>
+            </div>
+            <div class="row justify-content-center pb-2">
+                <figure class="m-3">
+                    <b-img src="/img/reciver1.jpg" fluid> </b-img>
+                </figure>
+            </div>
+            <div class="row pb-3">
+                <div class="col">
+                    <p class="indent">
+                        Максимальний тиск 11 бар(и).
+                    </p>
+                    <p class="indent">
+                        Комплект поставки:
+                    </p>
+                    <ul>
+                        <li>паспорт</li>
+                        <li>запобіжний клапан</li>
+                        <li>манометр</li>
+                        <li>кран зливу конденсату</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row justify-content-center pb-5">
+                <div class="col-lg-9 col-md-12 col-sm-12">
+                    <div class="table-responsive">
+                        <table class="table table-bordered ">
+                            <thead>
+                                <tr>
+                                    <th>Типорозмір ресивера</th>
+                                    <th>Номінальна місткість. м3</th>
+                                    <th>Робочий тиск (МПа)</th>
+                                    <th>Спосіб установки</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Р 20-Р 35</td>
+                                    <td>0,02 - 0,035</td>
+                                    <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 50.294</td>
+                                    <td>0,05</td>
+                                     <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 65.294</td>
+                                    <td>0.065</td>
+                                     <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                 <tr>
+                                    <td>Р 100.370</td>
+                                    <td>0.1</td>
+                                     <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 150.370</td>
+                                    <td>0.15</td>
+                                     <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 180.370</td>
+                                    <td>0.18</td>
+                                     <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 200.500</td>
+                                    <td>0.2</td>
+                                     <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 200.600</td>
+                                    <td>0.2</td>
+                                     <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                 <tr>
+                                    <td>Р 270.500</td>
+                                    <td>0.27</td>
+                                     <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 270.600</td>
+                                    <td>0.27</td>
+                                     <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 300.600</td>
+                                    <td>0.3</td>
+                                   <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 330.600</td>
+                                    <td>0.33</td>
+                                    <td>1,0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 500.600</td>
+                                    <td>0.5</td>
+                                    <td>1.0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 500.600</td>
+                                    <td>0.5</td>
+                                    <td>1.6</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>Р 750.800</td>
+                                    <td>0.75</td>
+                                    <td>1.0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                                                <tr>
+                                    <td>Р 900.800</td>
+                                    <td>0.9</td>
+                                    <td>1.0</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                                                <tr>
+                                    <td>Р 900.800</td>
+                                    <td>0.9</td>
+                                    <td>1.6</td>
+                                    <td>Гориз/вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>РВ 1150.900</td>
+                                    <td>1.15</td>
+                                    <td>1.0</td>
+                                    <td>Вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>РВ 1500.1000</td>
+                                    <td>1.5</td>
+                                    <td>1.0</td>
+                                    <td>Вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>РВ 2000.1200</td>
+                                    <td>2.0</td>
+                                    <td>1.0</td>
+                                    <td>Вертик</td>
+                                </tr>
+                                 <tr>
+                                    <td>РВ 3200.1200</td>
+                                    <td>3.2</td>
+                                    <td>1.0</td>
+                                    <td>Вертик</td>
+                                </tr>
+                                 <tr>
+                                    <td>РВ 4000.1400</td>
+                                    <td>4.0</td>
+                                    <td>1.0</td>
+                                    <td>Вертик</td>
+                                </tr>
+                                <tr>
+                                    <td>РВ 6300.1400</td>
+                                    <td>6.3</td>
+                                    <td>1.0</td>
+                                    <td>Вертик</td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </article>
     </div>
 </template>
 <script>
 export default {
-    layout:"default",
-}
+    layout: "default"
+};
 </script>
