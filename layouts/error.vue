6033dbfe1b987e2554b20f0a8dc6c1349e8d40e1
Anna Shaposhnikova <Shaposhnikova.anna@gmail.com>
2022-04-22 12:15:06 +0300

    404 page added

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="error-wrapper">
+   <div class="error-404">
+    
+    <h1 v-if="error.statusCode === 404">Page is not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <NuxtLink to="/">На головну</NuxtLink>
+  </div>
+</div>
+ 
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
